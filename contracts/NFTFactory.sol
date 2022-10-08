caed70be8396e3256a6dd319ea648776345c538d
Luis C Mogrovejo <luis.c.mogrovejo@gmail.com>
2022-10-07 22:53:25 -0500

    Superfluid

--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -3,22 +3,31 @@
 pragma solidity ^0.8.14;
 
 import "./ERC721.sol";
+import { 
+    IConstantFlowAgreementV1 
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+import {
+    CFAv1Library, ISuperfluid
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
 
 contract NF2U {
     uint256 public tokenCounter;
+
 
     constructor() {
         tokenCounter = 1;
     }
 
     function _mintNewNFT(
-        ISuperfluid host,
+        uint price,
+        address vault,
         string memory name,
         string memory symbol,
         string memory tokenUri
     ) public {
         uint256 newTokenId = tokenCounter;
-        new MyToken(host, name, symbol, tokenUri, newTokenId);
+        new MyToken(price, vault, name, symbol, tokenUri, newTokenId);
         tokenCounter += 1;
     }
 }
