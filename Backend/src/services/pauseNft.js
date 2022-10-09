648f5028a5b92094ca40bf315ebd9b6a3f0a72cf
Luis C Mogrovejo <luis.c.mogrovejo@gmail.com>
2022-10-08 23:01:01 -0500

    create

--- Backend/src/services/pauseNft.js
+++ Backend/src/services/pauseNft.js
@@ -33,11 +33,8 @@ const pauseNft = async (req, res) => {
   const { wallet, privateKey } = data;
   const provider = getWeb3(RPC_URL);
   const contract = getContract(provider, ERC721, contractAddress);
-  const web3 = new Web3;
-  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-  web3.eth.accounts.wallet.add(account);
-  web3.eth.defaultAccount = account.address;
-  const receipt = await contract.methods.pause()
+  // const { address: admin } = Web3.eth.accounts.wallet.add(privateKey);
+  const receipt = await contract.methods.pause().send({ from: wallet });
   console.log("-->> ", receipt);
 };
 
