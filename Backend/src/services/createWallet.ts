6c10cd6505470eb846cba9173997db51b0a7d846
ArturVargas <arturvargas9226@gmail.com>
2022-10-31 10:24:26 -0600

    add types

--- Backend/src/services/createWallet.ts
+++ Backend/src/services/createWallet.ts
@@ -4,35 +4,37 @@ import { Request, Response } from "express";
 import { createClient } from "@supabase/supabase-js";
 import Web3 from "web3";
 
+const createWallet = async (req: Request, res: Response) => {
+  const supabase = createClient(
+    "https://mnnbyrdnpuienzscjzjk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ubmJ5cmRucHVpZW56c2NqemprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUyNDAxNDUsImV4cCI6MTk4MDgxNjE0NX0.ynlyyTYvPKrNHDJW7mRj3_X41VSihmzuEkOO5OJF6P0"
+  );
 
-const createWallet  = async (req, res) => {
-    
-    const supabase = createClient('https://mnnbyrdnpuienzscjzjk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ubmJ5cmRucHVpZW56c2NqemprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUyNDAxNDUsImV4cCI6MTk4MDgxNjE0NX0.ynlyyTYvPKrNHDJW7mRj3_X41VSihmzuEkOO5OJF6P0');
-  
-    const provider_array = {
-        "mumbai": "https://attentive-ancient-spring.matic-testnet.discover.quiknode.pro/ffd31463498f334a11f8583f94c9e030e0b82c90/",
-        "optimism" : "https://muddy-methodical-shard.optimism-goerli.discover.quiknode.pro/1b6f71eb1516d728ed685e3feb911a318aa8837f/",
-        "skroll":"https://prealpha.scroll.io/l2/",
-        "goerli":"https://distinguished-light-breeze.ethereum-goerli.discover.quiknode.pro/39ed6ebac3cfbed9018e3298e383ef03820f023e/"
-    }
-    
-    var web3 = new Web3(new Web3.providers.HttpProvider(provider_array.goerli));   
-    var account = web3.eth.accounts.create();
+  const provider_array = {
+    mumbai:
+      "https://attentive-ancient-spring.matic-testnet.discover.quiknode.pro/ffd31463498f334a11f8583f94c9e030e0b82c90/",
+    optimism:
+      "https://muddy-methodical-shard.optimism-goerli.discover.quiknode.pro/1b6f71eb1516d728ed685e3feb911a318aa8837f/",
+    skroll: "https://prealpha.scroll.io/l2/",
+    goerli:
+      "https://distinguished-light-breeze.ethereum-goerli.discover.quiknode.pro/39ed6ebac3cfbed9018e3298e383ef03820f023e/",
+  };
+
+  var web3 = new Web3(new Web3.providers.HttpProvider(provider_array.goerli));
+  var account = web3.eth.accounts.create();
 
   // console.log(account.address);
   // console.log(account.privateKey);
   // console.log(req.body.email);
   //console.log(supabase);
 
-  const { error } = await supabase
-    .from("Users")
-    .insert([
-      {
-        email: req.body.email,
-        wallet: account.address,
-        privatekey: account.privateKey,
-      },
-    ]);
+  const { error } = await supabase.from("Users").insert([
+    {
+      email: req.body.email,
+      wallet: account.address,
+      privatekey: account.privateKey,
+    },
+  ]);
 
   if (!error) {
     return res.json({
